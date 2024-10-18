7e66308786dbc6756cd82c815b88ebecb09e4da0
Alireza Pourtaghi <alirezapt@protonmail.com>
2024-10-18 16:26:51 +0330

    Refactor handler.

--- src/main/java/com/lirezap/nex/net/AcceptConnectionHandler.java
+++ src/main/java/com/lirezap/nex/net/AcceptConnectionHandler.java
@@ -1,22 +1,44 @@
 package com.lirezap.nex.net;
 
 import com.lirezap.nex.context.AppContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.nio.channels.AsynchronousSocketChannel;
 import java.nio.channels.CompletionHandler;
+import java.nio.channels.ShutdownChannelGroupException;
 
 /**
+ * A completion handler that accepts incoming socket connections.
+ *
  * @author Alireza Pourtaghi
  */
 public final class AcceptConnectionHandler implements CompletionHandler<AsynchronousSocketChannel, AppContext> {
+    private static final Logger logger = LoggerFactory.getLogger(AcceptConnectionHandler.class);
 
     @Override
-    public void completed(final AsynchronousSocketChannel result, final AppContext context) {
-        // TODO: Complete implementation.
+    public void completed(final AsynchronousSocketChannel socket, final AppContext context) {
+        try {
+            context.nexServer().listen();
+        } catch (Exception ex) {
+            logger.error("listen call failed for next connection: {}", ex.getMessage());
+        }
+
+        handle(socket, context);
     }
 
     @Override
-    public void failed(final Throwable exc, final AppContext context) {
-        // TODO: Complete implementation.
+    public void failed(final Throwable th, final AppContext context) {
+        if (th.getCause() instanceof ShutdownChannelGroupException) {
+            logger.error("closed accepted connection because of channel group shutdown");
+        } else if (th.getCause() instanceof SecurityException) {
+            logger.error("closed accepted connection because of security restricted accept call");
+        } else {
+            logger.error("accept connection failed: {}", th.getMessage());
+        }
+    }
+
+    private void handle(final AsynchronousSocketChannel socket, final AppContext context) {
+        // TODO: Complete implementation!
     }
 }
